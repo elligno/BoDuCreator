cb30911d71dce005be1f5d747f8634709dc901ba
elligno <34111315+elligno@users.noreply.github.com>
2020-03-22 13:55:50 -0400

    Initial version

--- QtGuiAppBoduc.cpp
+++ QtGuiAppBoduc.cpp
@@ -3,5 +3,10 @@
 QtGuiAppBoduc::QtGuiAppBoduc(QWidget *parent)
 	: QMainWindow(parent)
 {
+	// just testing access to the global variable qApp
+	// since we are now inherit from QApplication
+	auto w_checkVersion = qApp->applicationVersion();
+	QDesktopWidget* w_desktop = qApp->desktop();
+
 	ui.setupUi(this);
 }
